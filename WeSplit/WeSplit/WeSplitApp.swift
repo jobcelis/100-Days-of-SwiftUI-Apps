be89640bae6ba20067338ebb84f98facf37848f7
Job Celis <145639726+jobcelis@users.noreply.github.com>
2025-06-17 15:06:38 -0700

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Job Celis on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
